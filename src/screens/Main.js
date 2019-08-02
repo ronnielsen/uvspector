0625cef1455fa2ee45ba1aed298f3bb7bcd7a50e
Ron Nielsen <ronaldnielsen@gmail.com>
2019-08-01 19:50:49 -0700

    init

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -8,12 +8,16 @@ function Main() {
       <div className="content">
         <div className="label">UV Index for</div>
           <div className="location-name">
-            Ronsville, CA
+            <a href="/search">Ronsville, CA</a>
           </div>
         <div className="uv-number">2</div>
         <div className="uv-status">Low</div>
       </div>
-      <div className="fixed-button"><FA name="map-marker" /></div>
+      <div className="fixed-button">
+        <a href="/search">
+          <FA name="search" />
+        </a>
+      </div>
     </div>
   );
 }
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import Geosuggest from 'react-geosuggest';
+import FA from 'react-fontawesome';
 
 function Search() {
+  const fixtures = [
+    {label: 'SF Bay Area', location: {lat: 37.8272, lng: 122.2913}},
+  ];
+
+  const google = window.google;
+
   return (
     <div className="App">
-      <div className="search">Hello</div>
+      <div className="search-container">
+        <div className="title">Search</div>
+        <Geosuggest
+          placeholder="Start typing!"
+          initialValue="San Jose, CA"
+          fixtures={fixtures}
+          location={new google.maps.LatLng(37.8272, 122.2913)}
+          radius="20" />
+        <div className="fixed-button invert">
+          <a href="/">
+            <FA name="check" />
+          </a>
+        </div>
+        <div className="fixed-button cancel">
+          <a href="/">
+            <FA name="times" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
