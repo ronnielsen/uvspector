8f8f5d390eedc74959029095370b9496f1fae0d4
Ron Nielsen <ronaldnielsen@gmail.com>
2019-08-02 10:57:23 -0700

    Updates

---
+++ precache-manifest.cca34b1107ba76de5376679e2ce74221.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d203a5e2c6a03878c963238434698c",
+    "url": "/uvspector/index.html"
+  },
+  {
+    "revision": "8a87d49c652b92db2c7f",
+    "url": "/uvspector/static/css/main.7a62d19c.chunk.css"
+  },
+  {
+    "revision": "1a1d10125dc2ca7d6aad",
+    "url": "/uvspector/static/js/2.e673ce72.chunk.js"
+  },
+  {
+    "revision": "8a87d49c652b92db2c7f",
+    "url": "/uvspector/static/js/main.c3b6d538.chunk.js"
+  },
+  {
+    "revision": "60962800f9b9b7187e21",
+    "url": "/uvspector/static/js/runtime~main.d3a02328.js"
+  }
+]);
