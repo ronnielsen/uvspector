e91a8a382ec96f4c453e63fc0dc883872eea0a11
Ron Nielsen <ronaldnielsen@gmail.com>
2019-08-02 10:56:50 -0700

    added Colors back, all rgba

---
+++ src/constants/Colors.js
@@ -0,0 +1,24 @@
+const tintColor = 'white';
+
+export default {
+  green: 'rgba(60,200,106,1)',
+  green10: 'rgba(60,200,106,.1)',
+  yellow: 'rgba(218,197,10,1)',
+  yellow10: 'rgba(218,197,10,.1)',
+  orange: 'rgba(255,111,76,1)',
+  orange10: 'rgba(255,111,76,.1)',
+  red: 'rgba(255,76,91,1)',
+  red10: 'rgba(255,76,91,.1)',
+  purple: 'rgba(141,76,255,1)',
+  purple10: 'rgba(141,76,255,.1)',
+  blue: 'rgba(76,131,255,1)',
+  blue10: 'rgba(76,131,255,.1)',
+  maroon: 'rgba(130,44,60,1)',
+  maroon10: 'rgba(130,44,60,.1)',
+  white90: 'rgba(255,255,255,.9)',
+  white50: 'rgba(255,255,255,.5)',
+  white25: 'rgba(255,255,255,.25)',
+  black90: 'rgba(0,0,0,.9)',
+  black50: 'rgba(0,0,0,.5)',
+  black25: 'rgba(0,0,0,.25)',
+};
