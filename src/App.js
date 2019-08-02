a878fc26dfd98803a9f8e6f5b49a87a87e592134
Ron Nielsen <ronaldnielsen@gmail.com>
2019-08-02 15:59:49 -0700

    Logic for colors

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function Users() {
 
 function App() {
   return (
-    <Router basename="public">
+    <Router>
       <div>
         <Route path="/" exact component={MainPage} />
         <Route path="/search/" component={SearchPage} />
--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import FA from 'react-fontawesome';
 import Colors from '../constants/Colors';
 
 function Main() {
 
+  const [uv, setUV] = useState(3);
+  const [myUV, setMyUV] = useState(Colors.green);
+  const [bg, setBg] = useState(Colors.green10);
+  const [status, setStatus] = useState('Mild');
+  const [location, setLocation] = useState('Green Hill Zone, WA')
+
+  const min = 1;
+  const max = 14;
+  const rand = min + (Math.random() * (max-min) - Math.random());
+
+  const handleSetUV = e => {
+    setUV(rand.toFixed(0))
+    console.log(uv, myUV, bg, status, location);
+    if (uv < 3) return setMyUV(Colors.green) + setBg(Colors.green10) + setStatus('Mild') + setLocation('Green Hill Zone, WA');
+    if (uv < 6) return setMyUV(Colors.yellow) + setBg(Colors.yellow10) + setStatus('Moderate') + setLocation('Yellow Brick Road, UK');
+    if (uv < 8) return setMyUV(Colors.orange) + setBg(Colors.orange10) + setStatus('High') + setLocation('Orange County, CA');
+    if (uv < 11) return setMyUV(Colors.red) + setBg(Colors.red10) + setStatus('Very High') + setLocation('Red Rock, UT');
+    return setMyUV(Colors.purple) + setBg(Colors.purple10) + setStatus('Extreme') + setLocation('Death Valley, CA');
+  }
+
   return (
-    <div className="App" style={{backgroundColor: Colors.purple10}}>
-      <div className="sol" style={{backgroundColor: Colors.purple}}>
+    <div className="App" style={{backgroundColor: bg}}>
+      <div className="sol" style={{backgroundColor: myUV}}>
       </div>
       <img className="sol shadow spin" src="shadows.svg" alt="sun-shadow"/>
       <div className="content">
         <div className="label">UV Index for</div>
           <div className="location-name">
-            <a href="/search">San Jose, CA</a>
+            <a href="/search">{location}</a>
           </div>
-        <div className="uv-number" style={{color: Colors.purple}}>11</div>
-        <div className="uv-status" style={{color: Colors.purple}}>Extreme</div>
+        <div className="uv-number" style={{color: myUV}}>{uv}</div>
+        <div className="uv-status" style={{color: myUV}}>{status}</div>
+      </div>
+      <div className="fixed-button buddy" style={{backgroundColor: Colors.black25}} onClick={handleSetUV}>
+        <FA name="random" />
       </div>
       <div className="fixed-button">
         <a href="/search">
