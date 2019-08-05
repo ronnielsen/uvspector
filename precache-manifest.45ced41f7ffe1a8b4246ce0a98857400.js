464b36fa7371ce8dbc3852038ec5b2fb94f81961
Ron Nielsen <ronaldnielsen@gmail.com>
2019-08-05 13:24:07 -0700

    Updates

---
+++ precache-manifest.45ced41f7ffe1a8b4246ce0a98857400.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac1e4ac52d893246d7e182453311d04",
+    "url": "/uvspector/index.html"
+  },
+  {
+    "revision": "76a9af274533d5136eea",
+    "url": "/uvspector/static/css/main.58f194b8.chunk.css"
+  },
+  {
+    "revision": "29d984086ce3e668b2ff",
+    "url": "/uvspector/static/js/2.fdb54c24.chunk.js"
+  },
+  {
+    "revision": "76a9af274533d5136eea",
+    "url": "/uvspector/static/js/main.e0eb072d.chunk.js"
+  },
+  {
+    "revision": "60962800f9b9b7187e21",
+    "url": "/uvspector/static/js/runtime~main.d3a02328.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a13899cab62f5741885d1be412134479.js"
+  "/uvspector/precache-manifest.45ced41f7ffe1a8b4246ce0a98857400.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/uvspector/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
